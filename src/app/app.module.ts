3c4a5deed1c202f23a14a1d9bae4e6788de6ca0c
ROHINI CHAVAN <rohinichavan@gmail.com>
2018-06-22 15:36:48 -0500

    created first new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ExampleComponent } from './components/example.component';
+import { ExampleTestComponent } from './components/example/example-test/example-test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExampleComponent,
+    ExampleTestComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/example.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-example',
+  template: `<h1>hello this is component</h1>`
+  
+})
+
+export class ExampleComponent {
+    title = 'app';
+  }
